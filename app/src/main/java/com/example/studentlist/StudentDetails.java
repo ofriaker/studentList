af88da3b4d257296917db611f514c76bd4c60168
amitsabag7 <amitsabag752@gmail.com>
2022-12-16 16:55:13 +0200

    add phone and address to students details

--- app/src/main/java/com/example/studentlist/StudentDetails.java
+++ app/src/main/java/com/example/studentlist/StudentDetails.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,6 +18,8 @@ public class StudentDetails extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         String name = bundle.getString("name");
         String id = bundle.getString("id");
+        String phone = bundle.getString("phone");
+        String address = bundle.getString("address");
         String url = bundle.getString("url");
         Boolean cb = bundle.getBoolean("cb");
 
@@ -25,10 +28,15 @@ public class StudentDetails extends AppCompatActivity {
         TextView phoneTv = findViewById(R.id.studentDetails_stPhone);
         TextView addressTv = findViewById(R.id.studentDetails_stAddress);
         ImageView img = findViewById(R.id.studentDetails_avatar);
+        CheckBox cbView = findViewById(R.id.studentDetails_cb);
 
         nameTv.setText(name);
         idTv.setText(id);
+        phoneTv.setText(phone);
+        addressTv.setText(address);
         img.setImageURI(Uri.parse(url));
+        cbView.setChecked(cb);
+
 
     }
 }
