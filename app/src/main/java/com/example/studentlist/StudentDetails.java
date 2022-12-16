cffc3d2e5671f3430c97bb504e430d277076773e
ofriaker <ofriaker@gmail.com>
2022-12-16 18:07:16 +0200

    making sure student detials are sent to edit when clicking edit

--- app/src/main/java/com/example/studentlist/StudentDetails.java
+++ app/src/main/java/com/example/studentlist/StudentDetails.java
@@ -2,11 +2,15 @@ package com.example.studentlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.example.studentlist.Model.Student;
 
 public class StudentDetails extends AppCompatActivity {
 
@@ -27,6 +31,7 @@ public class StudentDetails extends AppCompatActivity {
         TextView phoneTv = findViewById(R.id.studentDetails_stPhone);
         TextView addressTv = findViewById(R.id.studentDetails_stAddress);
         CheckBox cbView = findViewById(R.id.studentDetails_cb);
+        Button editBtn = findViewById(R.id.studentDetails_btn);
 
         nameTv.setText(name);
         idTv.setText(id);
@@ -34,6 +39,14 @@ public class StudentDetails extends AppCompatActivity {
         addressTv.setText(address);
         cbView.setChecked(cb);
 
-
+        editBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(StudentDetails.this, EditStudent.class);
+            intent.putExtra("name", name);
+            intent.putExtra("id", id);
+            intent.putExtra("phone", phone);
+            intent.putExtra("address", address);
+            intent.putExtra("cb", cb);
+            startActivity(intent);
+        });
     }
 }
